975d233afb4e148ed7e0a57dad4e3506c647b38e
savasana64 <128321910+savasana64@users.noreply.github.com>
2024-02-05 20:44:04 +0100

    Typing on screen using parameters

---
+++ HelloParameter.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numberOfCats = 5;
+    int numberOfDogs = 7;
+
+    cout << "total number of animals is: " << numberOfCats + numberOfDogs << endl;
+}
